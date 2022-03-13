69584aa79b2f42f7414b0f00ac56eb248e30fff3
Jonathan Haber <jryanhaber@gmail.com>
2022-03-13 11:59:11 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/accordion";
 
-function App() {
+const items = [
+  {
+    title: "what is react?",
+    content: "React is a Javascript..."
+  },
+  {
+    title: "what is react?.",
+    content: "React is a Javascript..."
+  },
+  {
+    title: "what is react?..",
+    content: "React is a Javascript..."
+  }
+];
+console.log(items);
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion items={items} />
     </div>
   );
-}
-
-export default App;
+};
