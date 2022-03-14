d7e6f15dd5442c24629ce8b1ff3b8d96e48f3b46
Jonathan Haber <jryanhaber@gmail.com>
2022-03-13 17:23:23 -0700

    async codein useEffect review

--- src/components/search.js
+++ src/components/search.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const Search = () => {
+  const [term, setTerm] = useState("");
+  console.log("I run with every render");
+
+  useEffect(() => {
+    console.log("I only after every render and at initial render");
+  }, [term]);
+
   return (
     <div>
-      <div class="ui container">
+      <div className="ui container">
         <br />
-        <h1>Search</h1>
+        <div className="ui form">
+          <h1>Search</h1>
+          <label>Enter Search Term</label>
 
-        <div class="ui input">
-          <input type="text" placeholder="Search..." />
+          <div>
+            <input
+              value={term}
+              onChange={e => setTerm(e.target.value)}
+              className="input"
+              type="text"
+              placeholder="Search..."
+            />
+          </div>
         </div>
       </div>
     </div>
