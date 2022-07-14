40c57f7e6373a2cdc60811f487998a2af6cf21aa
Jonathan Haber <jryanhaber@gmail.com>
2022-07-14 12:39:18 -0600

    widget buildout

---
+++ src/Link.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+
+// Created for testing purposes with Jest
+
+const STATUS = {
+  HOVERED: "hovered",
+  NORMAL: "normal"
+};
+
+const Link = ({page, children}) => {
+  const [status, setStatus] = useState(STATUS.NORMAL);
+
+  const onMouseEnter = () => {
+    setStatus(STATUS.HOVERED);
+  };
+
+  const onMouseLeave = () => {
+    setStatus(STATUS.NORMAL);
+  };
+
+  return (
+    <a
+      className={status}
+      href={page || "#"}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default Link;
